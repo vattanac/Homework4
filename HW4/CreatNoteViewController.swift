26f9ce0a9a88491f4b7aeda5b4b080c7a91b0c26
vattanac <vattanacsim99@gmail.com>
2018-12-11 22:47:24 +0700

    updat and clean code

--- HW4/CreatNoteViewController.swift
+++ HW4/CreatNoteViewController.swift
@@ -10,35 +10,65 @@ import UIKit
 import CoreData
 class CreatNoteViewController: UIViewController ,UITextViewDelegate {
     
+    //Textview as like Textfield for write Note Description
     @IBOutlet weak var TextFieldFack: UITextView!
+    //Textfiedl that behide Textview
     @IBOutlet weak var noteTextField: UITextField!
+    //Textfield write  Note Title
     @IBOutlet weak var titleTextfield: UITextField!
     
     var Noted:Note!
     var isUpdate = false
+    
+    
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
     lazy var  context = appDelegate.persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //get previous data frome each Note
         if isUpdate {
             titleTextfield.text = Noted.title
             TextFieldFack.text = Noted.des
             noteTextField.placeholder = ""
         }
         TextFieldFack.delegate = self
-        
-        
-
     }
-
+    
+    //function update Note
     func updateNote() {
             Noted.setValue(titleTextfield.text, forKey: "title")
             Noted.setValue(TextFieldFack.text, forKey: "des")
-        
             appDelegate.saveContext()
-        
     }
     
+    //Alert on TabbarbuttonItem
+    @IBAction func LeftTabbarItem(_ sender: Any) {
+        let alert = UIAlertController(title: "tools", message: nil, preferredStyle: .actionSheet)
+        
+        alert.addAction(UIAlertAction(title: "Take a photo", style: .default, handler: nil ))
+        alert.addAction(UIAlertAction(title: "Chose image", style: .default, handler: nil ))
+        alert.addAction(UIAlertAction(title: "Drawing", style: .default, handler: nil ))
+        alert.addAction(UIAlertAction(title: "Recording", style: .default, handler: nil ))
+        alert.addAction(UIAlertAction(title: "Checkbox", style: .default, handler: nil ))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    @IBAction func RightTabbarItem(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "Message", message: nil, preferredStyle: .actionSheet)
+        
+        alert.addAction(UIAlertAction(title: "Delete", style: .default, handler: nil ))
+        alert.addAction(UIAlertAction(title: "Make a copy", style: .default, handler: nil ))
+        alert.addAction(UIAlertAction(title: "Send", style: .default, handler: nil ))
+        alert.addAction(UIAlertAction(title: "Collabrators", style: .default, handler: nil ))
+        alert.addAction(UIAlertAction(title: "Label", style: .default, handler: nil ))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    //custome Textview as like textfield
     func textViewDidBeginEditing(_ textView: UITextView) {
         noteTextField.placeholder = ""
     }
@@ -48,6 +78,7 @@ class CreatNoteViewController: UIViewController ,UITextViewDelegate {
         }
     }
    
+    //Save data to Coredata
     func saveDate(){
         var note = Note(context: context)
         note.setValue(titleTextfield.text, forKey: "title")
@@ -55,6 +86,8 @@ class CreatNoteViewController: UIViewController ,UITextViewDelegate {
         
         appDelegate.saveContext()
     }
+    
+    //Study on condition click cell to update
     override func viewWillDisappear(_ animated: Bool) {
         if isUpdate == true {
             updateNote()
@@ -63,9 +96,4 @@ class CreatNoteViewController: UIViewController ,UITextViewDelegate {
             saveDate()
         }
     }
-    
-    
-    
-
-
 }
--- HW4/ViewController.swift
+++ HW4/ViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 import CoreData
+
+//start classs
 class ViewController: UIViewController, UITextViewDelegate, UICollectionViewDelegate , UICollectionViewDataSource {
-    
     
     
     @IBOutlet weak var collectionview: UICollectionView!
@@ -20,29 +21,12 @@ class ViewController: UIViewController, UITextViewDelegate, UICollectionViewDele
     var noteList = [Note]()
     lazy var context = appDelegate.persistentContainer.viewContext
     
-    func getData(){
-        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Note")
-        request.returnsObjectsAsFaults = false
-        do {
-            let result = try context.fetch(request)
-            for note in result as! [NSManagedObject]{
-                noteList.append(note as! Note)
-                print("######\(note)")
-                appDelegate.saveContext()
-            }
-        }catch{
-            
-        }
-    }
+   
     
     override func viewWillAppear(_ animated: Bool) {
-       
         noteList = [Note]()
-         getData()
+        getData()
         collectionview.reloadData()
-        
-        
-        
     }
     
     override func viewDidLoad() {
@@ -52,7 +36,7 @@ class ViewController: UIViewController, UITextViewDelegate, UICollectionViewDele
         collectionview.delegate = self
         collectionview.dataSource = self
         
-        
+        //make CollectionCiewCell look nicer
         var layout = self.collectionview.collectionViewLayout as! UICollectionViewFlowLayout
         layout.sectionInset = UIEdgeInsets(top: 0,left: 5,bottom: 0,right: 5)
         layout.minimumLineSpacing = 5
@@ -60,11 +44,11 @@ class ViewController: UIViewController, UITextViewDelegate, UICollectionViewDele
         layout.itemSize = CGSize(width: (vec.width - 20)/2 , height: vec.height/3)
         
         
-        
+         //custome NavigationBar
         self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
         self.navigationController?.navigationBar.shadowImage = UIImage()
         
-        //Guesture
+        //Create Guesture for Click On CollectionCiewCell
         let guesture:UILongPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(handleGuesture))
         
         guesture.minimumPressDuration = 0.3
@@ -72,22 +56,38 @@ class ViewController: UIViewController, UITextViewDelegate, UICollectionViewDele
         guesture.delaysTouchesBegan = true
         self.collectionview.addGestureRecognizer(guesture)
         
-        //
+        //Creat Guesture for Textfield in TabarItem
         dumyTextfield.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTextFieldTap))
       
         )
     }
     
+    //function get Data and add to Arraylist
+    func getData(){
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Note")
+        request.returnsObjectsAsFaults = false
+        do {
+            let result = try context.fetch(request)
+            for note in result as! [NSManagedObject]{
+                noteList.append(note as! Note)
+                //print("######\(note)")
+                appDelegate.saveContext()
+            }
+        }catch{
+            
+        }
+    }
     @IBAction func Noting(_ sender: Any) {
         
         
     }
     
+    //creat collectionviewcell
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return noteList.count
-        
     }
     
+    //set data to each collectionviewcell
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell_id", for: indexPath) as! CollectionViewCell
         
@@ -97,6 +97,7 @@ class ViewController: UIViewController, UITextViewDelegate, UICollectionViewDele
         return cell
     }
     
+    //condition when click collectionviewcell to update and current value on Note
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let creatNote = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "create") as! CreatNoteViewController
         creatNote.Noted = noteList[indexPath.row]
@@ -107,6 +108,7 @@ class ViewController: UIViewController, UITextViewDelegate, UICollectionViewDele
         
     }
     
+    //function Delete Note By index of collectionviewcell
     func DeleteNote(indexPath:IndexPath){
         let alert = UIAlertController(title: "Are you sure to delete?", message: nil, preferredStyle: .actionSheet)
         alert.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: { (action:UIAlertAction!) in
@@ -120,8 +122,13 @@ class ViewController: UIViewController, UITextViewDelegate, UICollectionViewDele
          self.present(alert, animated: true, completion: nil)
     }
     
-}
+}//End class
+
+
+//Extend for Guesture
 extension ViewController:UIGestureRecognizerDelegate{
+    
+    //handle guesture for click CollectionViewCell
     @objc func handleGuesture(guesturer:UILongPressGestureRecognizer){
         if(guesturer.state != UIGestureRecognizer.State.ended){
             return
@@ -129,10 +136,10 @@ extension ViewController:UIGestureRecognizerDelegate{
         let gestureLocate = guesturer.location(in: self.collectionview)
         if let indexPath: IndexPath = (self.collectionview.indexPathForItem(at: gestureLocate)){
             DeleteNote(indexPath: indexPath)
-            
         }
-        
     }
+    
+    //handle Textfield In Tabbar
     @objc func handleTextFieldTap(){
         let creteNoteVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "create") as! CreatNoteViewController
         navigationController?.pushViewController(creteNoteVC, animated: true)
